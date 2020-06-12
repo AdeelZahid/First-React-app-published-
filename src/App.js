34a1779165abb6b41e0f8164cedb23b453fa439e
Adeel Zahid <m.adeel2144@gmail.com>
2020-06-13 02:17:17 +0500

    node version and message updated

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-         Contact me on Facebook
+         Adeel Zahid on Facebook
         </a>
       </header>
     </div>
