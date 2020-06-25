7dbc472f4c8f05aeab659eed8f84f26ac799cc4a
Adeel Zahid <m.adeel2144@gmail.com>
2020-06-25 17:05:39 +0500

    changing some ...

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Dinner from './dinner.js'
+import Dinner from './dinner.js';
+import Message from './Message.js';
 
 function App() {
+  let [Count, setCount] = useState(5);
+  let [isMorning, setMorning] = useState(false);
   return (
     <div className="App">
       <header className="App-header">
@@ -18,8 +21,36 @@ function App() {
          Adeel Zahid on Facebook
         </a>
       </header>
+
+    <div className={`letCount ${isMorning ? 'dayNight' : ''}`}>
+      <Message counter={Count}/>
+      <h3> Good {isMorning ? 'Morning' : 'Night'} </h3>
+      <br />
+      <button onClick ={
+        ()=> setCount(++Count)
+      }> Increment </button>
+      <button onClick={
+        ()=>{
+          setCount(--Count);
+        }      }>
+        Decrement
+      </button>
+      <button onClick={
+        ()=>{
+          setCount(Count = 0);
+        }      }>
+        Set To Zero
+      </button>
+    </div>
+
+
       <footer>
           <Dinner dishName="Chicken Tikka" sweetDish="Kheer Pasta"/>
+      <button onClick={
+        ()=>{
+          setMorning(!isMorning);
+        }
+      }>Day Night Button</button>
       </footer>
     </div>
   );
---
+++ src/Message.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+
+
+export default function Message(props){
+    return(
+<p> Value of Count is : {props.counter} </p>
+    )
+}
